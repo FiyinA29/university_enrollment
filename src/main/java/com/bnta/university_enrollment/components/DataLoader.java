6219e994e920e320a646d7d4670ee0585c04c979
Fiyin Ayodele <fiyinayodele29@gmail.com>
2022-05-18 16:29:56 +0100

    dob added to students

--- src/main/java/com/bnta/university_enrollment/components/DataLoader.java
+++ src/main/java/com/bnta/university_enrollment/components/DataLoader.java
@@ -1,13 +1,19 @@
 package com.bnta.university_enrollment.components;
 
+import com.bnta.university_enrollment.models.Student;
+import com.bnta.university_enrollment.repositories.ClassRepository;
+import com.bnta.university_enrollment.repositories.StudentRepository;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 
 public class DataLoader implements ApplicationRunner {
+
+    private StudentRepository studentRepository;
+    private ClassRepository classRepository;
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        
+
     }
 }
--- src/main/java/com/bnta/university_enrollment/models/Student.java
+++ src/main/java/com/bnta/university_enrollment/models/Student.java
@@ -3,6 +3,7 @@ package com.bnta.university_enrollment.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 @Entity
@@ -11,10 +12,12 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column
+    @Column()
     private String firstName;
     @Column
     private String surname;
+    @Column(name = "date_of_birth")
+    private LocalDate dob;
     @ManyToMany(mappedBy = "students")
     @JsonIgnoreProperties({"students"})
     private List<Class> classes;
@@ -22,9 +25,10 @@ public class Student {
     protected Student() {
     }
 
-    public Student(String firstName, String surname) {
+    public Student(String firstName, String surname, LocalDate dob) {
         this.firstName = firstName;
         this.surname = surname;
+        this.dob = dob;
         this.classes = new ArrayList<Class>();
     }
 
@@ -46,6 +50,14 @@ public class Student {
 
     public void setSurname(String surname) {
         this.surname = surname;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
     }
 
     public List<Class> getClasses() {
@@ -70,6 +82,7 @@ public class Student {
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", surname='" + surname + '\'' +
+                ", dob=" + dob +
                 ", classes=" + classes +
                 '}';
     }
