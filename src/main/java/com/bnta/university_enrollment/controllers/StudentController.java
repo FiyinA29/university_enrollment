0e3798471b9f15951f2f7a2464473c47d87acfb1
Fiyin Ayodele <fiyinayodele29@gmail.com>
2022-05-18 21:49:08 +0100

    index controllers

--- src/main/java/com/bnta/university_enrollment/controllers/StudentController.java
+++ src/main/java/com/bnta/university_enrollment/controllers/StudentController.java
@@ -1,4 +1,26 @@
 package com.bnta.university_enrollment.controllers;
 
+import com.bnta.university_enrollment.models.Student;
+import com.bnta.university_enrollment.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("students")
 public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    //INDEX
+    @GetMapping
+    public ResponseEntity<List<Student>> getStudents(){
+        return new ResponseEntity<>(studentRepository.findAll(), HttpStatus.OK);
+    }
 }
--- src/main/java/com/bnta/university_enrollment/controllers/SubjectController.java
+++ src/main/java/com/bnta/university_enrollment/controllers/SubjectController.java
@@ -1,4 +1,26 @@
 package com.bnta.university_enrollment.controllers;
 
+import com.bnta.university_enrollment.models.Subject;
+import com.bnta.university_enrollment.repositories.SubjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("subjects")
 public class SubjectController {
+
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    //INDEX
+    @GetMapping
+    public ResponseEntity<List<Subject>> getSubjects(){
+        return new ResponseEntity<>(subjectRepository.findAll(), HttpStatus.OK);
+    }
 }
