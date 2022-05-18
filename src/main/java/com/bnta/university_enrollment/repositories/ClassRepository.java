cbebcbfd10b5ae4e9bdbac377331508480fc121b
Fiyin Ayodele <fiyinayodele29@gmail.com>
2022-05-18 16:13:12 +0100

    JPARepository

--- src/main/java/com/bnta/university_enrollment/repositories/ClassRepository.java
+++ src/main/java/com/bnta/university_enrollment/repositories/ClassRepository.java
@@ -1,4 +1,6 @@
 package com.bnta.university_enrollment.repositories;
 
-public interface ClassRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClassRepository extends JpaRepository<Class, Long> {
 }
--- src/main/java/com/bnta/university_enrollment/repositories/StudentRepository.java
+++ src/main/java/com/bnta/university_enrollment/repositories/StudentRepository.java
@@ -1,4 +1,7 @@
 package com.bnta.university_enrollment.repositories;
 
-public interface StudentRepository {
+import com.bnta.university_enrollment.models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
