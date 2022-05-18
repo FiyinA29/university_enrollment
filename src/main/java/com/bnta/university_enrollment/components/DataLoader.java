ef46d30133093c46ca59fa9792e8d03982b85ddf
Fiyin Ayodele <fiyinayodele29@gmail.com>
2022-05-18 16:17:39 +0100

    ApplicationRunner implementation

--- src/main/java/com/bnta/university_enrollment/components/DataLoader.java
+++ src/main/java/com/bnta/university_enrollment/components/DataLoader.java
@@ -1,4 +1,13 @@
 package com.bnta.university_enrollment.components;
 
-public class DataLoader {
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+
+public class DataLoader implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        
+    }
 }
