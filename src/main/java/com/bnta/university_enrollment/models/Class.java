07a92c1938c927625af2dc85a1a53dc963ee9239
Fiyin Ayodele <fiyinayodele29@gmail.com>
2022-05-18 15:58:48 +0100

    properties, constructors, getters&setters, and toString for models classes

--- src/main/java/com/bnta/university_enrollment/models/Class.java
+++ src/main/java/com/bnta/university_enrollment/models/Class.java
@@ -1,4 +1,58 @@
 package com.bnta.university_enrollment.models;
 
+import java.util.List;
+
 public class Class {
+
+    private Long id;
+    private String title;
+    private String description;
+    private List<Student> students;
+
+    protected Class() {
+    }
+
+    public Class(String title, String description, List<Student> students) {
+        this.title = title;
+        this.description = description;
+        this.students = students;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Class{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", students=" + students +
+                '}';
+    }
 }
--- src/main/java/com/bnta/university_enrollment/models/Student.java
+++ src/main/java/com/bnta/university_enrollment/models/Student.java
@@ -1,4 +1,66 @@
 package com.bnta.university_enrollment.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
+    private Long id;
+    private String firstName;
+    private String surname;
+    private List<Class> classes;
+
+    protected Student() {
+    }
+
+    public Student(String firstName, String surname) {
+        this.firstName = firstName;
+        this.surname = surname;
+        this.classes = new ArrayList<Class>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public List<Class> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(List<Class> classes) {
+        this.classes = classes;
+    }
+
+    public void addClass(Class subj){ //Couldn't use 'class' as a variable name
+        this.classes.add(subj);
+    }
+
+    public boolean removeClass(Class subj){
+        return this.classes.remove(subj);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", surname='" + surname + '\'' +
+                ", classes=" + classes +
+                '}';
+    }
 }
