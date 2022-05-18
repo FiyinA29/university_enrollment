a6291fdb48bc5db49d5276bdfb909c11a0225587
Fiyin Ayodele <fiyinayodele29@gmail.com>
2022-05-18 15:25:40 +0100

    Pom and Application Properties set up

---
+++ src/test/java/com/bnta/university_enrollment/UniversityEnrollmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.university_enrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityEnrollmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
